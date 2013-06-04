d3f6668bdf356fbe76329575bc704241974a4ee7
Roi Nir <roinir237@gmail.com>
2013-06-04 21:26:39 +0100

    MongoDB dependencies

--- web.js
+++ web.js
@@ -10,3 +10,10 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/mydb'; 
+s
